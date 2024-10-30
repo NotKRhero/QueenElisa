8398e3f907389106bf42e7af8e001c18bfdcc0d2
IQman <167598606+NotKRhero@users.noreply.github.com>
2024-10-30 21:32:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 IQbot Whatsapp`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://KRhero:@#gamingHERO@cluster0.w2kth.mongodb.net/', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
